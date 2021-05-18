e928bd054a8044a029416811f4902bf1ef88711e
mesba <mesbaul333@gmail.com>
2021-05-18 19:08:36 +0600

    index file initiated

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ * Title: Markdown Blog
+ *  Description: Simple Markdown blog app using express , mongodb and so on...
+ *  Author: Mohammad Mesbaul Haque
+ * Date: 18/05/2021
+ */
+
+// Dependencies.
+
+
+
+// Module scaffolding.
+
+
+// main functions or objects.
+
+
+
+// export the module.
