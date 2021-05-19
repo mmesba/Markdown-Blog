2c833b45cfbbc6e47877f0c9621de8a13868766d
mesba <mesbaul333@gmail.com>
2021-05-19 15:56:52 +0600

    models dir added and schema file iniated

---
+++ models/Article.js
@@ -0,0 +1,27 @@
+/*
+ * Title: Article Schema.
+ * Description: Article schema for Markdown blog app.
+ * Author: Mohammad Mesbaul Haque (github profile: https://github.com/Mohammad-Mesbaul-Haque )
+ * Date: 19/05/2021
+ */
+// Dependencies.
+const mongoose = require('mongoose');
+
+
+
+
+
+// Main functions.
+const articleSchema = mongoose.Schema({
+    title: {
+        type: String
+    }
+})
+
+
+
+
+
+
+// export the module.
+// module.exports = 
