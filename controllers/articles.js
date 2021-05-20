874cd06c6e0e32f2cfcbb1ff84bda0214a455a10
mesba <mesbaul333@gmail.com>
2021-05-20 19:53:27 +0600

    edit functionality added and some refactoring

--- controllers/articles.js
+++ controllers/articles.js
@@ -29,31 +29,41 @@ router.get('/:slug', async (req, res)=>{
     res.render('articles/show', {article: article})
 })
 
-router.post('/',async (req, res)=>{
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try {
-      article =   await article.save();
-      //if successfully save article , redirect
-      res.redirect(`/articles/${article.slug}`)
-      // it will redirect to the show page where the newly saved data will be arrived
+router.post('/',async (req, res, next)=>{
+   req.article = new Article();
+   next();
+}, saveArticleAndRedirect('new'))
 
-    } catch (err) {
-        // console.error(err.message);
-        res.render('articles/new', {article: article})
-    }
-})
-
-// router.put()
+router.put('/:id',async (req, res, next)=>{
+    req.article = await Article.findById(req.params.id)
+    next();
+ }, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async(req, res)=>{
     await Article.findByIdAndDelete(req.params.id);
     res.redirect('/');
 })
 
+//common function for post and put route
+// because they are almost identical
+function saveArticleAndRedirect(path) {
+    return async(req, res)=>{
+        let article = req.article
+            article.title= req.body.title;
+            article.description= req.body.description;
+            article.markdown= req.body.markdown;
+        
+        try {
+          article =   await article.save();
+          //if successfully save article , redirect
+          res.redirect(`/articles/${article.slug}`)
+          // it will redirect to the show page where the newly saved data will be arrived
+    
+        } catch (err) {
+            // console.error(err.message);
+            res.render(`articles/${path}`, {article: article})
+        }
+    }}
 
 
 // export the module.
